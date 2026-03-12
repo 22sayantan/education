2afdb8fa4462cd9f90b7e6b206c9af4756d3e9b3
sayantan22 <sayantandolui97@gmail.com>
2026-03-12 21:54:32 +0530

    update 4.0

--- Educational/views.py
+++ Educational/views.py
@@ -102,10 +102,9 @@ def mockTest(request):
     today = today.strftime("%d/%m/%Y")
         
     if request.method == 'POST':
-        subject = request.POST.get('subject')
-        print(subject)
-        filtered_mcq = MCQ.objects.filter(subject = subject)
-        print(filtered_mcq)
+        selected_sub = request.POST.get('drop-down-value-sub')
+        # selected_level = request.POST.get('drop-down-value-level')
+        print(selected_sub)
         '''
         del_id = request.POST.get('delete_id')
         if del_id:
@@ -114,10 +113,31 @@ def mockTest(request):
                 cursor.execute("WITH ordered AS ( SELECT id, ROW_NUMBER() OVER (ORDER BY id) AS rn FROM Educational_mcq ) UPDATE Educational_mcq SET id = ( SELECT rn FROM ordered WHERE ordered.id = Educational_mcq.id );")
                 cursor.execute("DELETE FROM sqlite_sequence WHERE name='Educational_mcq';")
         '''
-        return render(request,'mockTest.html',{'queses' : queses,'stmts':T_F_queses,'longques':longques,'date':today})
+
+        context = {
+        'selected_value': selected_value,
+        }
+
     else:
-        print('nothing')
-        return render(request,'mockTest.html',{'queses' : queses,'stmts':T_F_queses,'longques':longques,'date':today})
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * from Educational_mcq order by random() limit 8;")
+            queses = cursor.fetchall()
+            cursor.close()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * from Educational_true_false order by random() limit 5;")
+            T_F_queses = cursor.fetchall()
+            cursor.close()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * from Educational_longques order by random() limit 1;")
+            longques = cursor.fetchall()
+            cursor.close()
+            
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT distinct subject from Educational_mcq;")
+            subjects=cursor.fetchall()
+            cursor.close()
+        
+    return render(request,'mockTest.html',{'queses' : queses,'stmts':T_F_queses,'longques':longques,'date':today,'subjects':subjects})
 
 def courses(request):
     with connection.cursor() as cursor:
